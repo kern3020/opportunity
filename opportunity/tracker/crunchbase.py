d022582484ca5051b19be5edc4d2c180ced618c4
John Kern <kern3020@gmail.com>
2015-02-08 09:00:32 -0800

    updated for new v2 APIs

--- opportunity/tracker/crunchbase.py
+++ opportunity/tracker/crunchbase.py
@@ -1,82 +1,109 @@
 '''
-CrunchBase is the free database of technology companies, people, and
-investors that anyone can edit. This is a proxy for the Crunchbase API.
-This is good for people looking for jobs in technology. Keep an eye
-open for other data sources for different disciplines.
-
-http://api.crunchbase.com/v/1/<namespace>/<permalink>.js?api_key=(your_acess_key)
-
-crunchbase has the following namespaces:
-    company
-    person
-    financial-organization
-    product
-    service-provider
-
-With the company namespace, the permalink is generally a company name.
-
-For the implementation we just need.
-results['name']
-results['offices']  # list of addresses.
-results['crunchbase_url']
-results['homepage_url']
-results['overview']
-
-An error looks like
-{"error": "Sorry, we could not find the record you were looking for."}
+CrunchBase allows access to there database of technology companies,
+people, and investors. This is a proxy for the Crunchbase API.  This
+is good for people looking for jobs in technology. Keep an eye open
+for other data sources for different disciplines.
 
 '''
 
-from django.utils import six
-
 import os
 import json
+import logging
 
-
-if six.PY3:
-    from  urllib.parse import quote_plus as quote_plus
-    from  urllib.request import urlopen as urlopen
-else:
-    from urllib import quote_plus as quote_plus
-    from urllib2 import urlopen as urlopen
+# Using python 3 specific APIs
+from http.client import responses
+from urllib.error import HTTPError
+from urllib.parse import quote
+from urllib.request import urlopen
 
 
 class CrunchProxy():
     '''
-    This class does the heavy lifting.
+    This class does the heavy lifting. The view shouldn't know about the keys
+    in the json returned by the cruncbase API.
     '''
     def __init__(self):
-        self.base_url = "http://api.crunchbase.com/v/1/"
+        self.base_url = "http://api.crunchbase.com/v/2"
         self.api_key = os.environ['CRUNCHBASE_KEY']
+        self.co_request_template = "{0}/{1}?user_key={2}"
+        self.generic_request_template = "{0}/organizations?name={1}&organization_types=company&user_key={2}&page=1"
 
-    def get_company_details(self, company):
-        """
-        This method returns dict based on json data from crunchbase.
-        If the api is reporting an error, check for an error key.
-        This method can throw an exception. Caller should handle it.
-        """
-        permalink = "company"
-        url_request_co = (self.base_url + permalink + "/"
-                          + quote_plus(company)
-                          + ".js?api_key=" + self.api_key)
-        results = {} 
-        results = urlopen(url_request_co)
-        if results.headers['content-type'] == 'text/javascript; charset=utf-8':
-            results = json.loads(results.read().decode("utf-8"))
-        return results
+        # results
+        self._name = None
+        self._description = None
+        self._street_address = None
+        self._city = None
+        self._state = None
+        self._zip = None
+        self._country = None
+        self._website = None
 
-    def generic_query(self, term):
-        """
-        Given a term, search through the crunchbase database. This returns a
-        list of dictionaries. The 'namespace' key tells you if is a company,
-        product, etc. This method can raise exceptions. Caller should handle
-        that.
-        """
-        url_request_query = (self.base_url + "search.js?query="
-                             + quote_plus(term)
-                             + "&api_key=" + self.api_key)
+    def __str__(self):
+        return "{0}\n{1}, {2}\n{3}".format(
+            self._name, self._city, self._state, self._description)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def street_address(self):
+        return self._street_address
+
+    @property
+    def city(self):
+        return self._city
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def zip(self):
+        return self._zip
+
+    @property
+    def website(self):
+        return self._website
+
+    def query(self, query):
         results = {}
-        results = urlopen(url_request_query)
-        if results.headers['content-type'] == 'text/javascript; charset=utf-8':
-            results = json.loads(results.read().decode('utf-8'))
-        return results['results']
+        try:
+            # see if we can find the exact name as name of org
+            url_request = self.generic_request_template.format(
+                self.base_url, quote(query), self.api_key)
+            results = json.loads(urlopen(url_request).readall().decode("utf-8"))
+            if len(results['data']['items']) == 0:
+                self.log_protocol_err('{0} - no matches found'.format(query))
+            else:
+                # on success, retrieve path to and submit query
+                path = results['data']['items'][0]['path']  # assumption first record is best.
+                url_request = self.co_request_template.format(
+                    self.base_url, quote(path),
+                    self.api_key)
+                results = json.loads(urlopen(url_request).readall().decode("utf-8"))
+                if 'response' in results['data'] and results['data']['response'] is False:
+                    self.log_protocol_err(results['data']['error'])
+                else:
+                    self._name = results['data']['properties']['name']
+                    self._description = results['data']['properties']['description']
+                    self._website = results['data']['properties']['homepage_url']
+                    list_of_offices = results['data']['relationships']['offices']['items']
+                    zz = list_of_offices
+                    if len(list_of_offices) > 0:
+                        office = list_of_offices[0]
+                        self._street_address = ""
+                        if office['street_1']:
+                            self._street_address += office['street_1']
+                        if office['street_2']:
+                            self._street_address += office['street_2']
+                        self._city = office['city']
+                        self._state = office['region']
+                        self._country = office['country']
+                        self._zip = office['postal_code']
+        except HTTPError as e:
+            logging.error(str.format("HTTP Error: {0} - {1}", e.code,
+                                     responses[e.code]))
+
+    def log_protocol_err(self, msg):
+        logging.error(msg)
--- opportunity/tracker/crunchbase_test.py
+++ opportunity/tracker/crunchbase_test.py
@@ -3,12 +3,14 @@
 The crunchbase API is used to populate the company template if data is
 available. Here are the tests.
 
+python -m unittest crunchbase_test.FetchFromCrunch.test_normal
+
 '''
 
 from django.utils import unittest
 
-from .models import Company
-from .views import populateCompany
+import crunchbase
+
 
 class FetchFromCrunch(unittest.TestCase):
     def test_normal(self):
@@ -16,36 +18,32 @@ class FetchFromCrunch(unittest.TestCase):
         The simpliest case is a single token with no special characters
         which matches a specific company in crunchbase.
         '''
-        co = Company()
-        co.name = "Solum"
-        populateCompany(co)
-        self.assertEqual(co.city, "San Francisco")
+        proxy = crunchbase.CrunchProxy()
+        proxy.query("Syapse")
+        self.assertEqual(proxy.city, "Palo Alto")
 
     def test_encoding(self):
         '''
         What happens if the string needs to be encode?
         '''
-        co = Company()
-        co.name = "Red Hat"
-        populateCompany(co)
-        self.assertEqual(co.city, "Raleigh")
+        proxy = crunchbase.CrunchProxy()
+        proxy.query("Pacific Biosciences")
+        self.assertEqual(proxy.city, "Menlo Park")
 
     def test_matches_multiple(self):
         '''
         What happens if a company has multiple offices?
         '''
-        co = Company()
-        co.name = "IBM"
-        populateCompany(co)
-        self.assertEqual(co.city, "Armonk")
+        proxy = crunchbase.CrunchProxy()
+        proxy.query("IBM")
+        self.assertEqual(proxy.city, "Armonk")
 
     def test_no_match(self):
         '''
         What happens if there is no match with crunchbase?
         '''
-        co = Company()
-        name = "pirulito"
-        co.name = name
-        populateCompany(co)
-        self.assertEqual(co.name, name)
-        self.assertEqual(co.city, "")
+        name = '9329eb13-912c-4336-8c35-903ebc84f971'
+        proxy = crunchbase.CrunchProxy()
+        proxy.query(name)
+        self.assertEqual(proxy.name, None)
+        self.assertEqual(proxy.city, None)
